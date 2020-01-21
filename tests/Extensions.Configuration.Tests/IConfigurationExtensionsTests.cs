e73721514bcce52a6a39570b201e8b9921f3caeb
JanDonnermayer <jan@donnermayer.de>
2020-01-21 10:17:49 +0100

    add test cases

--- tests/Extensions.Configuration.Tests/IConfigurationExtensionsTests.cs
+++ tests/Extensions.Configuration.Tests/IConfigurationExtensionsTests.cs
@@ -25,7 +25,7 @@ namespace Extensions.Configuration.Tests
             SubstitutionFormatOptions.DollarBrackets;
         private const string OPTIONS_3_PREFIX = "$(";
         private const string OPTIONS_3_SUFFIX = ")";
-        
+
         private const SubstitutionFormatOptions OPTIONS_4 =
             SubstitutionFormatOptions.Percent;
         private const string OPTIONS_4_PREFIX = "%";
@@ -42,6 +42,7 @@ namespace Extensions.Configuration.Tests
         [TestCase(OPTIONS_1)]
         [TestCase(OPTIONS_2)]
         [TestCase(OPTIONS_3)]
+        [TestCase(OPTIONS_4)]
         public void Test_ResolveValue_ZeroResolveSteps_OneKeyPerStep_ResolvesCorrectly(
             SubstitutionFormatOptions options
         )
@@ -65,6 +66,7 @@ namespace Extensions.Configuration.Tests
         [TestCase(OPTIONS_1)]
         [TestCase(OPTIONS_2)]
         [TestCase(OPTIONS_3)]
+        [TestCase(OPTIONS_4)]
         public void Test_ResolveValue_ZeroResolveSteps_OneKeyPerStep_KeyEmptyString_ResolvesCorrectly(
             SubstitutionFormatOptions options
         )
@@ -142,8 +144,13 @@ namespace Extensions.Configuration.Tests
             Assert.AreEqual(VALUE_2 + VALUE_2, actualValue);
         }
 
-
-        public void Test_ResolveValue_TwoResolveSteps_OneKeyPerStep_ResolvesCorrectly()
+        [TestCase(OPTIONS_1, OPTIONS_1_PREFIX, OPTIONS_1_SUFFIX)]
+        [TestCase(OPTIONS_2, OPTIONS_2_PREFIX, OPTIONS_2_SUFFIX)]
+        [TestCase(OPTIONS_3, OPTIONS_3_PREFIX, OPTIONS_3_SUFFIX)]
+        [TestCase(OPTIONS_4, OPTIONS_4_PREFIX, OPTIONS_4_SUFFIX)]
+        public void Test_ResolveValue_TwoResolveSteps_OneKeyPerStep_ResolvesCorrectly(
+            SubstitutionFormatOptions options, string prefix, string suffix
+        )
         {
             // Arrange
             const string KEY_1 = "key1";
@@ -173,12 +180,17 @@ namespace Extensions.Configuration.Tests
         }
 
 
-        [Test]
-        public void Test_ResolveValue_InfiniteSteps_OneKeyPerStep_ThrowsInvalidOperationException()
+        [TestCase(OPTIONS_1, OPTIONS_1_PREFIX, OPTIONS_1_SUFFIX)]
+        [TestCase(OPTIONS_2, OPTIONS_2_PREFIX, OPTIONS_2_SUFFIX)]
+        [TestCase(OPTIONS_3, OPTIONS_3_PREFIX, OPTIONS_3_SUFFIX)]
+        [TestCase(OPTIONS_4, OPTIONS_4_PREFIX, OPTIONS_4_SUFFIX)]
+        public void Test_ResolveValue_InfiniteSteps_OneKeyPerStep_ThrowsInvalidOperationException(
+            SubstitutionFormatOptions options, string prefix, string suffix
+        )
         {
             // Arrange
             const string KEY_1 = "key1";
-            const string VALUE_1 = OPTIONS_1_PREFIX + KEY_1 + OPTIONS_1_SUFFIX;
+            string VALUE_1 = prefix + KEY_1 + suffix;
 
             Mock.Get(configurationMock)
                 .SetupGet(cfg => cfg[It.Is<string>(k => k == KEY_1)])
@@ -186,18 +198,22 @@ namespace Extensions.Configuration.Tests
 
             // AssertThrows
             Assert.Throws<InvalidOperationException>(
-                () => configurationMock.ResolveValue(KEY_1)
+                () => configurationMock.ResolveValue(KEY_1, options)
             );
         }
 
 
-        [Test]
-        public void Test_ResolveValue_InfiniteSteps_TwoKeysPerStep_ThrowsInvalidOperationException()
+        [TestCase(OPTIONS_1, OPTIONS_1_PREFIX, OPTIONS_1_SUFFIX)]
+        [TestCase(OPTIONS_2, OPTIONS_2_PREFIX, OPTIONS_2_SUFFIX)]
+        [TestCase(OPTIONS_3, OPTIONS_3_PREFIX, OPTIONS_3_SUFFIX)]
+        [TestCase(OPTIONS_4, OPTIONS_4_PREFIX, OPTIONS_4_SUFFIX)]
+        public void Test_ResolveValue_InfiniteSteps_TwoKeysPerStep_ThrowsInvalidOperationException(
+            SubstitutionFormatOptions options, string prefix, string suffix
+        )
         {
             // Arrange
             const string KEY_1 = "key1";
-            const string VALUE_1 = OPTIONS_1_PREFIX + KEY_1 + OPTIONS_1_SUFFIX 
-                + OPTIONS_1_PREFIX + KEY_1 + OPTIONS_1_SUFFIX;
+            string VALUE_1 = prefix + KEY_1 + suffix + prefix + KEY_1 + suffix;
 
             Mock.Get(configurationMock)
                 .SetupGet(cfg => cfg[It.Is<string>(k => k == KEY_1)])
@@ -205,7 +221,7 @@ namespace Extensions.Configuration.Tests
 
             // AssertThrows
             Assert.Throws<InvalidOperationException>(
-                () => configurationMock.ResolveValue(KEY_1)
+                () => configurationMock.ResolveValue(KEY_1, options)
             );
         }
 
