adf50b4e94b8b2c5b97df8039c262ce2d1f1e5fe
JanDonnermayer <jan@donnermayer.de>
2020-03-29 21:15:29 +0200

    rearrange imports

--- src/Extensions.Configuration.Sources.Object/ConfigurationProviders/ObjectConfigurationProvider.cs
+++ src/Extensions.Configuration.Sources.Object/ConfigurationProviders/ObjectConfigurationProvider.cs
@@ -1,7 +1,7 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Extensions.Configuration;
 
