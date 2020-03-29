695d803fd6e518b7ca5adea164a82396bd71376c
JanDonnermayer <jan@donnermayer.de>
2020-03-29 05:59:04 +0200

    Add test and docs

--- src/Extensions.Configuration.Sources.Object/TreeFolder.cs
+++ src/Extensions.Configuration.Sources.Object/TreeFolder.cs
@@ -7,25 +7,35 @@ namespace Extensions.Configuration.Sources.Object
 {
     internal static class TreeFolder
     {
-        public static IEnumerable<KeyValuePair<ImmutableArray<TKey>, TValue>> Fold<TKey, TValue>(
-            this IEnumerable<KeyValuePair<TKey, dynamic>> source,
-            Func<dynamic, TValue> leafConverter
+        /// <summary>
+        /// Converts a sequence of trees of <see cref="KeyValuePair"/> into a flat sequence of
+        /// <see cref="KeyValuePair"/>, aggregating the keys into sequences.
+        /// </summary>
+        /// <Example>
+        /// { { K1, { K11, V1 }, { K2, V2 } } => { { [ K1, K11 ], V1 }, { K2, V2 } }
+        /// </Example>
+        public static IEnumerable<KeyValuePair<IEnumerable<TKey>, TValue>> Fold<TKey, TValue>(
+            this IEnumerable<KeyValuePair<TKey, object>> source,
+            Func<object, TValue> leafConverter
         )
         {
-            IEnumerable<KeyValuePair<ImmutableArray<TKey>, TValue>> FoldInternal(ImmutableArray<TKey> path, dynamic value) =>
+            IEnumerable<KeyValuePair<IEnumerable<TKey>, TValue>> FoldInternal(ImmutableList<TKey> path, object value) =>
                 value switch
                 {
                     IEnumerable<KeyValuePair<TKey, dynamic>> seq =>
-                        seq.SelectMany(kvp => FoldInternal(path.Add(kvp.Key), kvp.Value)),
-                    var val => new[] {
-                        new KeyValuePair<ImmutableArray<TKey>, TValue>(
-                            key: path,
-                            value: leafConverter(value)
-                        )
-                    }
+                        seq.SelectMany(
+                            kvp => FoldInternal(path.Add(kvp.Key), kvp.Value)
+                        ),
+                    var val =>
+                        new[] {
+                            new KeyValuePair<IEnumerable<TKey>, TValue>(
+                                key: path,
+                                value: leafConverter(value)
+                            )
+                        }
                 };
 
-            return FoldInternal(ImmutableArray<TKey>.Empty, source);
+            return FoldInternal(ImmutableList<TKey>.Empty, source);
         }
     }
 }
