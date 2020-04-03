8a934cddc4ec2d37ed0045400da3857eb096cd71
JanDonnermayer <jan@donnermayer.de>
2020-04-03 19:42:32 +0200

    inernalize member

--- src/Extensions.Configuration.Sources.Objects/Extensions/IConfigurationProviderExtensions.cs
+++ src/Extensions.Configuration.Sources.Objects/Extensions/IConfigurationProviderExtensions.cs
@@ -1,10 +1,15 @@
 using System;
-using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Extensions.Configuration
 {
-    public static class IConfigurationProviderExtensions
+    internal static class IConfigurationProviderExtensions
     {
+        /// <summary>
+        /// Tries to get the configuration value for the specified key.
+        /// </summary>
+        /// <param name="provider">The <see cref="IConfigurationProvider"/> from which to get the value.</param>
+        /// <param name="key">The key, whose value to get.</param>
+        /// <returns>(true, value) if a value for the specified key was found, otherwise (false, null).</returns>
         public static (bool contains, string? value) TryGetValue(this IConfigurationProvider provider, string key)
         {
             if (provider is null)
